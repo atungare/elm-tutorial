93078536608fdf553c27abf791f2de33ee4158e5
atungare <ajay.tungare@gmail.com>
2016-04-03 19:59:37 -0400

    3.4 multiple actions

---
+++ ManyActions.elm
@@ -0,0 +1,49 @@
+module Main (..) where
+
+import Html exposing (Html)
+import Mouse
+import Keyboard
+
+type alias Model = { count: Int }
+
+type Action = NoOp | MouseClick | KeyPress
+
+initialModel : Model
+initialModel =
+  { count = 0 }
+
+view : Model -> Html
+view model =
+  model.count
+  |> toString
+  |> Html.text
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    MouseClick ->
+      { model | count = model.count + 1 }
+    KeyPress ->
+      { model | count = model.count - 1 }
+    _ ->
+      model
+
+mouseClickSignal : Signal.Signal Action
+mouseClickSignal =
+  Signal.map (\_ -> MouseClick) Mouse.clicks
+
+keyPressSignal : Signal.Signal Action
+keyPressSignal =
+  Signal.map (\_ -> KeyPress) Keyboard.presses
+
+actionSignal : Signal.Signal Action
+actionSignal =
+  Signal.merge mouseClickSignal keyPressSignal
+
+modelSignal : Signal.Signal Model
+modelSignal =
+  Signal.foldp update initialModel actionSignal
+
+main : Signal.Signal Html
+main =
+  Signal.map view modelSignal
