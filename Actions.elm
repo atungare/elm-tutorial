da7448b6aa6f2d00ed3fe7a3fa9dc668fb48b870
atungare <ajay.tungare@gmail.com>
2016-04-03 19:55:46 -0400

    3.3 actions

---
+++ Actions.elm
@@ -0,0 +1,38 @@
+module Main (..) where
+
+import Html exposing (Html)
+import Mouse
+
+type alias Model = { count: Int }
+
+type Action = NoOp | Increase
+
+initialModel : Model
+initialModel =
+  { count = 0 }
+
+view : Model -> Html
+view model =
+  model.count
+  |> toString
+  |> Html.text
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increase ->
+      { model | count = model.count + 1 }
+    NoOp ->
+      model
+
+actionSignal : Signal.Signal Action
+actionSignal =
+  Signal.map (\_ -> Increase) Mouse.clicks
+
+modelSignal : Signal.Signal Model
+modelSignal =
+  Signal.foldp update initialModel actionSignal
+
+main : Signal.Signal Html
+main =
+  Signal.map view modelSignal
