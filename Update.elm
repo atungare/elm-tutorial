fbf5d09f5b37ac3aa70131a816d0133a3041ac71
atungare <ajay.tungare@gmail.com>
2016-04-03 19:49:56 -0400

    3.2 update

---
+++ Update.elm
@@ -0,0 +1,30 @@
+module Main (..) where
+
+import Html exposing (Html)
+import Mouse
+
+type alias Model = { count: Int }
+
+initialModel : Model
+initialModel =
+  { count = 0 }
+
+view : Model -> Html
+view model =
+  model
+  |> .count
+  |> toString
+  |> Html.text
+
+update : () -> Model -> Model
+update _ model =
+  { model | count = model.count + 1 }
+
+
+modelSignal : Signal.Signal Model
+modelSignal =
+  Signal.foldp update initialModel Mouse.clicks
+
+main : Signal.Signal Html
+main =
+  Signal.map view modelSignal
