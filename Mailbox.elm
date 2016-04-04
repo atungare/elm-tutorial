feba57b111c6dbfde068c961d79ab968a6ca0bcf
atungare <ajay.tungare@gmail.com>
2016-04-03 20:11:24 -0400

    3.6 mailbox revisited

--- Mailbox.elm
+++ Mailbox.elm
@@ -3,19 +3,39 @@ module Main (..) where
 import Html exposing (Html)
 import Html.Events as Events
 
-view : Signal.Address String -> String -> Html
-view address msg =
+type alias Model = { count : Int }
+
+type Action = NoOp | Increase
+
+initialModel : Model
+initialModel =
+  { count = 0 }
+
+view : Signal.Address Action -> Model -> Html
+view address model =
   Html.div
     []
-    [ Html.div [] [ Html.text msg ]
+    [ Html.div [] [ Html.text (toString model.count) ]
     , Html.button
-      [ Events.onClick address "Hello" ]
+      [ Events.onClick address Increase ]
       [ Html.text "Click me" ]
     ]
 
-mb : Signal.Mailbox String
-mb = Signal.mailbox ""
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increase ->
+      { model | count = model.count + 1 }
+    NoOp ->
+      model
+
+mb : Signal.Mailbox Action
+mb = Signal.mailbox NoOp
+
+modelSignal : Signal.Signal Model
+modelSignal =
+  Signal.foldp update initialModel mb.signal
 
 main : Signal Html
 main =
-  Signal.map (view mb.address) mb.signal
+  Signal.map (view mb.address) modelSignal
